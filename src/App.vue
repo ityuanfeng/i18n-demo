c5334947197a0b0184e04dc6368fb8592a65d394
zouyuanfeng <891178455@qq.com>
2024-05-06 21:06:16 +0800

    格式化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <script setup lang="ts">
 import {useI18n} from 'vue-i18n'
+import {ref} from "vue";
 
 const {t} = useI18n()
 console.log(t('hello'))
+
+
+const langs = ['zh-CN', 'en']
+const lang = localStorage.getItem('lang') || navigator.language
+const curIndex = ref(langs.indexOf(lang))
+const changeLang = (index: number) => {
+    curIndex.value = index
+    localStorage.setItem('lang', langs[index])
+    window.location.reload()
+}
 </script>
 
 <template>
-    <div>
-        <p>{{ t('hello') }}</p>
-        <p>{{ t('list', ['hello', 'world']) }}</p>
+    <div class="root">
+        <div class="lang">
+            <div class="title">切换语言</div>
+            <ul>
+                <li :class="index===curIndex ?'active':''" @click="changeLang(index)" v-for="(item,index) in langs">{{
+                        item
+                    }}
+                </li>
+            </ul>
+        </div>
+        <div class="code">
+            <ul>
+                <li>默认：{{ $t('hello') }}</li>
+                <li>列表参数：{{ $t('list', ['hello', 'world']) }}</li>
+                <li>对象参数：{{ $t('obj', {name: 'Jack', age: 18}) }}</li>
+                <li>特殊字符: {{ $t('address', { account: 'foo', domain: 'domain.com' }) }}<br>
+                特殊字符：{、}、@、$、|</li>
+                <li>引用：{{ $t('message.linked') }}</li>
+                <li>内置修改器：{{ $t('message.homeAddress') }}，修改后： {{ $t('message.missingHomeAddress') }} <br> upper【转大写】、lower【转小写】、capitalize【首字母大写】</li>
+                <li>自定义修改器：{{ $t('message.custom_modifier')}}</li>
+            </ul>
+        </div>
+        <div></div>
+        <div></div>
     </div>
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
+<style lang="scss" scoped>
+.root {
+    width: 100%;
 
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
+    .lang {
+        width: 300px;
+        text-align: left;
 
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+        .title {
+            margin-left: 30px;
+        }
+
+        .active {
+            color: red;
+        }
+        li{
+            cursor: pointer;
+        }
+    }
+
+    .code {
+        text-align: left;
+    }
 }
 </style>
--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,7 +1,7 @@
 import {createI18n} from 'vue-i18n'
 
 function loadLang() {
-    const lang = import.meta.glob('./lang/*.json', { eager: true })
+    const lang = import.meta.glob('./lang/*.json', {eager: true})
     const messages: Record<string, any> = {};
     for (const key in lang) {
         const matched = key.match(/([A-Za-z0-9-_]+)\./i);
@@ -15,7 +15,11 @@ function loadLang() {
 
 export default createI18n({
     // locale: navigator.language,
-    locale:'en',
+    locale: localStorage.getItem('lang') || navigator.language,
     legacy: false, // 设置为 false，启用 composition API 模式
-    messages: loadLang()
+    messages: loadLang(),
+    // 自定义翻译函数
+    modifiers: {
+        snakeCase: (str: string) => str.split(' ').join('_')
+    }
 })
